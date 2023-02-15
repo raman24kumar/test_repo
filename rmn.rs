2e8e6edf1d96a178181fbf18da94499234ad44a4
raman24kumar <125288960+raman24kumar@users.noreply.github.com>
2023-02-15 23:58:29 +0530

    Add files via upload

---
+++ rmn.rs
@@ -0,0 +1,23 @@
+
+fn main() {
+    let condition = true;
+    let number = if !condition { 5 } else { 6 };// return types of both if and else blocks should be same.
+
+    println!("The value of number is: {}",number);
+
+   
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+    /*Using too many else if expressions can clutter your code, so if you have more than one, 
+    you might want to refactor your code. 
+    Chapter 6 describes a powerful Rust branching construct called match for these cases.*/
+
+}
